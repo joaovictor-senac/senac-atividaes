9fec4844e43909bb04a9a70eaa26d63c9a1db9d7
joaovictor-senac <joao.98971@aluno.pr.senac.br>
2022-04-14 16:38:34 -0300

    for conceitos; otimizando o loop;

--- realizandoTeste.java
+++ realizandoTeste.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class realizandoTeste {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int x = 1;
+
         for (int i = 1; i < 11; i++) {
-            System.out.println(x);
-            x++;
+            System.out.println(i);
         }
     }
 }
