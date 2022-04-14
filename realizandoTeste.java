869f3322677b27cfc69843eba439d3fcc06ff185
joaovictor-senac <joao.98971@aluno.pr.senac.br>
2022-04-14 16:31:21 -0300

    for conceitos; adicionando o Scanner

--- realizandoTeste.java
+++ realizandoTeste.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class realizandoTeste {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int x = 1;
         for (int i = 1; i < 11; i++) {
             System.out.println(x);
