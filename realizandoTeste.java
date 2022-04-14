18fbbd40b9e62a4ca9d8383ac5efc083b7e39b7e
joaovictor-senac <joao.98971@aluno.pr.senac.br>
2022-04-14 16:14:06 -0300

    for conceitos

--- realizandoTeste.java
+++ realizandoTeste.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class realizandoTeste {
+    public static void main(String args[]) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println("helloy word");
+        }
+    }
 }
