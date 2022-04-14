d814ffe0380b9c8cc2bfdf1aefb877622cb5e903
joaovictor-senac <joao.98971@aluno.pr.senac.br>
2022-04-14 17:17:12 -0300

    for off conceitos.

--- Loop.java
+++ Loop.java
@@ -1,8 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Loop {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        List nome = new ArrayList();
+        nome.add("joao");
+        nome.add("valentina");
+        nome.add("kaio");
+        nome.add("julio");
+//        System.out.println(nome);
+
+        //For off
+        for (Object Lista : nome) {
+            if (Lista.equals("kaio")) {
+                System.out.println(Lista);
+                System.out.println("comprimentar o kaio");
+            }
+        }
     }
 }
