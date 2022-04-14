c6a773efc2e2cdab8da3b8dad48c2d12fac06dbf
joaovictor-senac <joao.98971@aluno.pr.senac.br>
2022-04-14 16:22:20 -0300

    for conceitos; que conta até 10

--- realizandoTeste.java
+++ realizandoTeste.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 public class realizandoTeste {
-    public static void main(String args[]) {
-        for (int i = 1; i < 10; i++) {
-            System.out.println("helloy word");
+    public static void main(String[] args) {
+        int x = 1;
+        for (int i = 1; i < 11; i++) {
+            System.out.println(x);
+            x++;
         }
     }
 }
