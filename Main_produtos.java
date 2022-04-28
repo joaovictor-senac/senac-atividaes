3f728f7e0ab6aa9c88282f78dbc084adb9669e97
joaovictor-Senac <joao.98971@alunos.pr.senac.br>
2022-04-28 14:06:44 -0300

    terminio da aula de ontem

---
+++ Main_produtos.java
@@ -0,0 +1,11 @@
+public class Main_produtos {
+   /* public static void main(String[] args) {
+          Produtos produtos = new Produtos();
+
+
+          produtos.setProduto("telefone");
+          produtos.setDescricao("de ultima geração");
+
+
+    }*/
+}
---
+++ Produtos.java
@@ -0,0 +1,66 @@
+public class Produtos {
+
+
+    private String produto;
+
+    private String descricao;
+
+    private String dataDeVecimento;
+
+    private int valor;
+
+    private int valorPromocao;
+
+    private String data;
+
+
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataDeVecimento() {
+        return dataDeVecimento;
+    }
+
+    public void setDataDeVecimento(String dataDeVecimento) {
+        this.dataDeVecimento = dataDeVecimento;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValorPromocao() {
+        return valorPromocao;
+    }
+
+    public void setValorPromocao(int valorPromocao) {
+        this.valorPromocao = valorPromocao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
